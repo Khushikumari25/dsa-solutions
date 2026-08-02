c6ae8fc54b7d01ba64760bbfd9691f8116ad79ea
Khushi Kumari <162818128+Khushikumari25@users.noreply.github.com>
2026-08-02 14:08:37 +0530

    Added Stone Game - LeetCode

--- LeetCode/Stone-Game.java
+++ LeetCode/Stone-Game.java
@@ -1,6 +1,21 @@
 class Solution {
     public boolean stoneGame(int[] arr) {
-        return true;
+        int max = 0;
+        int n = arr.length;
+        int alice1 = 0;
+        int alice2 = 0;
+        int bob = 0;
+        for(int i = 0 ; i < n ; i+=2){
+            alice1 += arr[i];
+            alice2 += arr[n-1];
+            max = Math.max(alice1 , alice2);
+            bob = arr[i+1];
+        }
+        if(max > bob){
+            return true ;
+        }else{
+            return false;
+        }
         
     }
 }
