4b2977d0f2d513559c7242b2931681bb8d7acaf4
Khushi Kumari <162818128+Khushikumari25@users.noreply.github.com>
2026-08-06 20:27:32 +0530

    Added Smallest Divisible Digit Product I - LeetCode

--- LeetCode/Smallest-Divisible-Digit-Product-I.java
+++ LeetCode/Smallest-Divisible-Digit-Product-I.java
@@ -10,17 +10,25 @@ class Solution {
             
         for(int i = n ; i <= 100 ; i++){
             String s = String.valueOf(i);
-            if( s.length() == 1){
-                return t;
+            if( s.length() == 1 ){
+                for( int k = n ; k <= 9; k++){
+                    if( k % t == 0){
+                        return k;
+                    }
+                
+                }
             }
-        
-            for(int j = 0 ; j < s.length(); j++){
+        if(s.length() == 2){
+             for(int j = 0 ; j < s.length(); j++){
                 int mul = s.charAt(0) * s.charAt(1);
                 if(mul % t == 0 ){
                     return Integer.parseInt(s);
                 }
                 break;
             }
+
+        }
+           
         
             
         }
